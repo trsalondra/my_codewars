8acabb01e5e899c9fc0335ba5f22770ce9e0650e
trsalondra <trsalondra@gmail.com>
2023-01-12 12:21:52 -0600

    Anagram Detection Kata complete

---
+++ anagramDetection.js
@@ -0,0 +1,33 @@
+// write the function isAnagram
+// var isAnagram = function(test, original) {
+//     let testArr = test.toLowerCase().split("").sort()
+//     let originalArr = original.toLowerCase().split("").sort()
+
+//     return testArr.join("").toString() === originalArr.join("").toString()
+
+// }
+
+
+// REFACTORED 1
+// var isAnagram = function(test, original) {
+//     return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("")
+// }
+
+// REFACTORED 2
+// var isAnagram = function(test, original) {
+//     test = test.toLowerCase().split("").sort().join("")
+//     original = original.toLowerCase().split("").sort().join("")
+
+//     return test === original
+// };
+
+// REFACTORED 3
+const isAnagram = (test, original) => { test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("")}
+
+console.log(isAnagram("foefet", "toffee")) // true, 'The word "foefet" is an anagram of "toffee"')
+console.log(isAnagram("Buckethead", "DeathCubeK")) // true, 'The word "Buckethead" is an anagram of "DeathCubeK"')
+console.log(isAnagram("Twoo", "WooT")) // true, 'The word "Twoo" is an aconsole.log
+
+console.log(isAnagram("dumble", "bumble")) // false, 'Characters do not match for test case "dumble", "bumble"')
+console.log(isAnagram("ound", "round")) // false, 'Missing characters for test case "ound", "round"')
+console.log(isAnagram("apple", "pale")) // false, 'Same letters, but different count')
--- theHashtagGenerator.js
+++ theHashtagGenerator.js
@@ -17,8 +17,8 @@
 //     }
 // }
 
-// refactored 
 
+// REFACTORED
 function generateHashtag(str) {
     if (!str || !(str.trim().length > 0)) return false
 
@@ -33,3 +33,14 @@ function generateHashtag(str) {
       let newStr = newArr.join("").toString()
       return (newStr.length > 140 ? false : newStr)
 }
+
+console.log(generateHashtag("")) // false, "Expected an empty string to return false")
+console.log(generateHashtag(" ".repeat(200))) // false, "Still an empty string")
+console.log(generateHashtag("Do We have A Hashtag")) // "#DoWeHaveAHashtag", "Expected a Hashtag (#) at the beginning.")
+console.log(generateHashtag("Codewars")) // "#Codewars", "Should handle a single word.")
+console.log(generateHashtag("Codewars Is Nice")) // "#CodewarsIsNice", "Should remove spaces.")
+console.log(generateHashtag("Codewars is nice")) // "#CodewarsIsNice", "Should capitalize first letters of words.")
+console.log(generateHashtag("code" + " ".repeat(140) + "wars")) // "#CodeWars")
+console.log(generateHashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat")) // false, "Should return false if the final word is longer than 140 chars.")
+console.log(generateHashtag("a".repeat(139))) // "#A" + "a".repeat(138) "Should work")
+console.log(generateHashtag("a".repeat(140))) // false, "Too long")
